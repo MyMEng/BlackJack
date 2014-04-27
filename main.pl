267b8866b83744f87011f703e2214a94d2593218
Kacper <ks1591@bristol.ac.uk>
2014-04-27 22:25:06 +0100

    AI module to do

--- main.pl
+++ main.pl
@@ -15,9 +15,9 @@ play :-
 	initDeal(Table, NewDeck, Shuffled), % deal the table
 	write('Initial table state:'), nl,
 	printGame(Table),
-	theGame(Table, NewDeck).
+	theGame(Table, NewDeck, 1).
 
-theGame(Table, Deck) :-
+theGame(Table, Deck, Ask) :-
 	userPlayer(U),
 
 	checkBJ(Allowence, _, Table), % check for initial BlackJack
@@ -26,13 +26,17 @@ theGame(Table, Deck) :-
 	((	U = 1,
 		getNoPlayers(R), % get player ID
 		elementN(Elem, Allowence, R),
-		( \+ (Elem = -1)  -> askCard(Answer) % ask if player has not already lost
-		; otherwise       -> Answer = 0
+		( (\+ (Elem = -1), Ask = 1)  -> askCard(Answer) % ask if player has not already lost
+		; otherwise                  -> Answer = 0
 		),
-		( Answer = 1 -> addCard(NewTable, NewDeck, Deck, Table, R)
-		; Answer = 0 -> ( NewTable = Table, NewDeck = Deck )
+
+		% do not allow to take card once you have refused
+
+		( Answer = 1 -> (addCard(NewTable, NewDeck, Deck, Table, R), Ask1 = Ask)
+		; Answer = 0 -> ( NewTable = Table, NewDeck = Deck, Ask1 is 0 )
 		)
 	 ) ; (
+	 	Ask1 = Ask,
 	 	U = 0,
 	 	NewTable = Table,
 	 	NewDeck = Deck
@@ -45,7 +49,7 @@ theGame(Table, Deck) :-
 	croupierAI(NNTable, NNDeck, NTable, NDeck), % do the AI magic
 	printGame(NNTable),
 	\+ checkTheEnd( Allowence ), % for the moment end the game-normally shuffle and new deal
-	theGame(NNTable, NNDeck).
+	theGame(NNTable, NNDeck, Ask1).
 
 % check whether all players are done playing
 checkTheEnd( [ -1|Aa ] ) :-
