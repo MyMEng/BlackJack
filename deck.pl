2052ac644baa36c3fea69db4cb76af03de3b8ba3
Kacper <ks1591@bristol.ac.uk>
2015-09-06 18:54:17 +0100

    Pldoc documented and cleaned; Prolog package formatted

--- deck.pl
+++ deck.pl
@@ -1,4 +1,12 @@
-% Deck class %
+/** <module> Predicates for handling a deck of card
+
+This module contains configuration and predicates responsible for handling
+deck(s) of cards through the game.
+
+@author Kacper Sokol
+@license GPL
+*/
+
 :- module( deck,
   [decks/1,
    shuffleMode/1,
@@ -12,24 +20,50 @@
 
 :- use_module(blackJack). % players
 
-%% define number of decks
+%% decks(-N) is det.
+%
+% Defines the number of decks used in the game.
+%
+% @param N  Number of decks used in the game.
+%
 decks(1).
-%% decide whether shuffle is made with coin toss or it is deterministic
-%% shuffleMode(deterministic). % deterministic
-shuffleMode(random). % random
 
-%% define suits
-%% clubs (♣), diamonds (♦), hearts (♥) and spades (♠) 
-%% suits(d).
+%% shuffleMode(-Type) is det.
+%
+% Defines the type of deck shuffling used in the game; can be either:
+%  * deterministic, or
+%  * random.
+%
+% The deterministic shuffling splits the pile in half randomly selects the half 
+% and merge halves interchangeably. The random shuffling also splits the deck 
+% but then tosses a coin on each card merge which card should go first.
+%
+% @param Type  Type of shuffling used in the game.
+%
+shuffleMode(random).
+
+%% suits(-Type) is det.
+%
+% Defines suits of cards:
+%  * clubs(♣),
+%  * diamonds(♦),
+%  * hearts(♥) and
+%  * spades(♠).
+%
+% @param Type  Type of card's suit.
+%
 suits(♦).
-%% suits(h).
 suits(♥).
-%% suits(s).
 suits(♠).
-%% suits(c).
 suits(♣).
 
-%% define ranks
+%% ranks(-Rank) is det.
+%
+% Defines the possible ranks of card: ace, 2, 3, 4, 5, 6, 7, 8, 9, 10, jack, 
+% queen, king.
+%
+% @param Rank  Rank of card.
+%
 ranks(  ace).
 ranks(    2).
 ranks(    3).
@@ -43,18 +77,26 @@ ranks(   10).
 ranks( jack).
 ranks(queen).
 ranks( king).
-%% ace   :- 1; 11.
-%% jack  :-    10.
-%% queen :-    10.
-%% king  :-    10.
 
-% functions definitions
-%% card representation
-%% c( suit, rank ).
+%% card(+Suit, +Rank) is det.
+%% card(-Suit, -Rank) is nondet.
+%
+% Represents a card.
+%
+% @param Suit  Suit of card.
+% @param Rank  Rank of card.
+%
 card( Suit, Rank ) :-
 	suits( Suit ), ranks( Rank ).
 
-%% Get the value of card
+%% value(Value, +card(+S, +R)) is det.
+%
+% Converts card predicate to its value.
+%
+% @param Value  Value of given card.
+% @param S      Suit of card.
+% @param R      Rank of card.
+%
 value( Value, card( S, R ) ) :-
 	( S = ♣; S = ♦; S = ♥; S = ♠ ), !,
 	( R = ace   -> ( Value is 11 ; Value is 1 )
@@ -64,7 +106,13 @@ value( Value, card( S, R ) ) :-
 	; otherwise -> Value is R
 	).
 
-%% calculate card value | Hand is a list of *card* objects
+%% score(-Score, +Hand) is det.
+%
+% Calculates value of player's hand.
+%
+% @param Score  Value of given hand.
+% @param Hand   A list of `card` predicates.
+%
 score( Score, Hand ) :- % Hand
 	score( Score, Hand, 0 ).
 score( Score, [Card|Hand], V ) :-
@@ -74,7 +122,13 @@ score( Score, [Card|Hand], V ) :-
 score( Score, [], V ) :-
 	Score is V.
 
-%% generate predefined number deck---return a list with all cards
+%% deck(-Deck) is det.
+%
+% Generates deck(s) based on their predefined number (`decks` parameter in 
+%	`deck` module).
+%
+% @param Deck  Deck(s) generated based on system parameters.
+%
 deck(Deck) :-
 	decks(No),
 	deck(Deck, [], No).
@@ -87,20 +141,37 @@ deck(Deck, Holder, No) :-
 	append(Holder, New, NewHolder),
 	deck(Deck, NewHolder, Np).
 
-%% Return list without element E
+%% woN(-Out, +E, +In) is det.
+%
+% Returns the `In` list without element `E`.
+%
+% @param Out  The list with specified element removed.
+% @param E    An element ot be removed from the input list.
+% @param In   List with the element to be removed.
+%
 woN(Out, E, In) :-
 	select(E, In, Out), !.
 
-%% deal the cards to N players---we begin with two cards each + shuffler
-%%  + player(/no-player mode)
-%% Return list of lists with cards---hands
-%% % hand(L) :- [ card(), card(), card(), card(),... ]
+%% initDeal(-Table, -NewDeck, +Deck) is det.
+%
+% Deals the cards from the `Deck` to all players defined by `players` predicate 
+% defined in `blackJack` module. The initial deal consists of two cards given 
+% to each player & two cards for the house. This predicate returns the new deck 
+% without just dealt cards and the table i.e. list of lists containing cards of 
+% each player.
+%
+% @param Table    List of cards held by all player (list of lists).
+% @param NewDeck  Deck(s) of card with removed cards that were used for initial 
+%                 deal.
+% @param Deck     Deck(s) of cards used for initial deal.
+%
 initDeal(Table, NewDeck, Deck) :-
 	players(P),
 	userPlayer(Q),
 	AtTable is P + 1 + Q,
 	%% CardDeals is AtTable * 2, % deal 2 cards for each player
 	initDeal(Table, NewDeck, [], Deck ,Deck, AtTable, 1).
+%
 initDeal(Table, Deck, Table, Deck, _, AtTable, Current) :-
 	AtTable is Current - 1, !.
 initDeal(Table, NewDeck, TemporaryTable, DissortingD, Deck, AtTable, Current) :-
@@ -114,10 +185,15 @@ initDeal(Table, NewDeck, TemporaryTable, DissortingD, Deck, AtTable, Current) :-
 	append(TemporaryTable, [Person], Table2),% generate i-th player
 	initDeal(Table, NewDeck, Table2, Deck2, Deck, AtTable, Next).
 
-%% DEFINE COUPLE OF SHUFFLE MODES
-%% shuffle a deck by RIFLE-shuffle split into half and toss a coin
-%% for each card to decide whether it goes on the bottom or on the top
-%% or just do it in the right order
+%% shuffle(-Shuffled, +Deck, +N) is det.
+%
+% Manages shuffling: decides which shuffling algorithm to use based on type 
+% defined by `shuffleMode` predicate defined in `deck` package.
+%
+% @param Shuffled  The shuffled deck (list of `card` predicate).
+% @param Deck      The deck to be shuffled (list of `card` predicate).
+% @param N         Number of shuffles.
+%
 shuffle(Shf, Shf, 0) :-
 	!.
 shuffle(Shuffled, Deck, N) :-
@@ -134,7 +210,16 @@ shuffle(Shuffled, Deck, N) :-
 	N1 is N - 1,
 	shuffle( Shuffled, Forward, N1 ).
 
-%% rifle shuffle two piles deterministically
+%% rifleDet(-Out, +A, +B) is det.
+%
+% Rifle-shuffle two piles of cards deterministically: split the deck into two 
+% parts and merge them one by one. The only random bit here is whether the 
+% shuffle starts with pile A or pile B.
+%
+% @param Out  Merged deck (list of predicates `card`).
+% @param A    1st part of deck to be merged (list of predicates `card`).
+% @param B    2nd part of deck to be merged (list of predicates `card`).
+%
 rifleDet(Out, A, B) :-
 	random(0, 2, Rand), % decide whether left pile goes on top or bottom,
 	( Rand = 0 -> rifleDet(Out, [], A, B)
@@ -153,7 +238,16 @@ rifleDet(Out, Em, [A1|A2], []) :-
 rifleDet(Out, Out, [], []) :-
 	!.
 
-%% rifle shuffle two piles randomly
+%% rifleRan(-Out, +A, +B) is det.
+%
+% Rifle-shuffle two piles of cards with *random* card selection i.e. split the 
+% deck into two parts and merge them by tossing a coin for each card to decide 
+% whether it goes on the top or bottom of the merged pile.
+%
+% @param Out  Merged deck (list of predicates `card`).
+% @param A    1st part of deck to be merged (list of predicates `card`).
+% @param B    2nd part of deck to be merged (list of predicates `card`).
+%
 rifleRan(Out, A, B) :-
 	random(0, 2, Rand), % decide whether left pile goes on top or bottom
 	rifleRan(Out, [], A, B, Rand).
@@ -176,7 +270,17 @@ rifleRan(Out, Em, [A1|A2], [], Rand) :-
 rifleRan(Out, Out, [], [], _) :-
 	!.
 
-%% generate two piles counters
+%% getPiles(-A, -B, +Half) is det.
+%
+% Generates two piles counters: if the number of cards in the deck is **odd** 
+% then it is impossible to split it equally. This predicate takes a float 
+% number and outputs two numbers one a half bigger and one a half smaller to 
+% create two integers. In general 2*`Half`=`A`+`B`.
+%
+% @param A     Number rounded to an integer based on above rule.
+% @param B     Number rounded to an integer based on above rule.
+% @param Half  A number to be rounded based on above rule.
+%
 getPiles(A, B, Half) :-
 	float(Half),
 	random(0,2,Rand), % decide whether round up left or right pile
@@ -187,7 +291,21 @@ getPiles(A, B, Half) :-
 getPiles(Half, Half, Half) :-
 	integer(Half).
 
-%% Deal additional card to player *i*
+%% addCard(-NewDeal, -NewDeck, +OldDeck, +OldDeal, +PlayerNo) is det.
+%
+% Deals additional card to player with sequential number `PlayerNo`. After the 
+% deal it returns new deck and new hand of given player.
+%
+% @param NewDeal   List of cards held by all player (list of lists) **after** 
+%                  adding a new card.
+% @param NewDeck   **New** list of cards remaining in the deck after current 
+%                  round.
+% @param OldDeck   Current list of cards in the deck after.
+% @param OldDeal   List of cards held by all player (list of lists) **before** 
+%                  adding a new card.
+% @param PlayerNo  An id of the player counting from 1 which is to be dealt a 
+%                  new card.
+%
 addCard(NewDeal, NewDeck, [Card|NewDeck], OldDeal, PlayerNo) :-
 	split(A, [X|C], OldDeal, PlayerNo), % get sublist
 	append(X, [Card], Y), % extend sublist
